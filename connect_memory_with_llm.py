e55370f0020568878545794c70f90d229b555107
aryantyagi0 <122510840+aryantyagi0@users.noreply.github.com>
2025-08-29 01:23:50 -0700

    Add files via upload

---
+++ connect_memory_with_llm.py
@@ -0,0 +1,55 @@
+import os
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# HF Token
+HF_TOKEN = os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID = "google/flan-t5-base"   # safer choice unless you have a paid endpoint
+
+# Step 1: Setup LLM
+def load_llm(huggingface_repo_id):
+    llm = HuggingFaceEndpoint(
+        repo_id=huggingface_repo_id,
+        huggingfacehub_api_token=HF_TOKEN,
+        temperature=0.5,
+        max_new_tokens=512
+    )
+    return llm
+
+# Step 2: Custom Prompt
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context.
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    return PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+
+# Load FAISS Database
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+# Create QA Chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(HUGGINGFACE_REPO_ID),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k':3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+# Run Query
+user_query = input("Write Query Here: ")
+response = qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
